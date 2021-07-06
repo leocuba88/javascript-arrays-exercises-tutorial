470b183ac7f7a2a063651f009c25ecde597fb197
leocuba88 <leocuba88@hotmail.com>
2021-07-06 23:24:48 +0000

    done

--- exercises/25-Techno-beat/app.js
+++ exercises/25-Techno-beat/app.js
@@ -1,3 +1,30 @@
+let lyricsGenerator = (arr) => {
+    let arrBoom = [];
+        arr.forEach((element, index )=> {
+            if (element == 0){
+                arrBoom.push("Boom")
+            } else if (element == 1){
+                arrBoom.push("Drop the base")
+            } if (arr[index] == 1  && arr[index - 1] == 1 && arr[index - 2] == 1){
+                arrBoom.push("!!!Break the base!!!")
+            }
+        });
+        return arrBoom.join(" ")
+    }
+
+
+
+// let lyricsGenerator = (arr) => {
+//     arr.forEach(element => {
+//         if (element == 0){
+//             console.log("Boom")
+//         } else if (element == 1){
+//             console.log("Drop the base")
+//         }
+//     });
+// }
+
+
 
 // test Data
 console.log(lyricsGenerator([0,0,1,1,0,0,0]))
